42259581addb88e6b9f24a7dc8a083a65dedd94f
samirbaracat <samirbaracat@gmail.com>
2025-01-12 08:18:24 -0400

    Criando o projeto

---
+++ src/main/java/com/samdevweb/apiopenfeign/ApiopenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.samdevweb.apiopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiopenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiopenfeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdevweb/apiopenfeign/ApiopenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdevweb.apiopenfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiopenfeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
